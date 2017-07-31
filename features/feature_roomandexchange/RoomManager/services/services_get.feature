a57c2609caa49afe5e545f0332f99e7c1a84d8b1
666nonak666 <norman.antezana@fundacion-jala.org>
2017-07-31 21:47:08 +0000

    meeting schema

--- features/feature_roomandexchange/RoomManager/services/services_get.feature
+++ features/feature_roomandexchange/RoomManager/services/services_get.feature
@@ -1,6 +1,31 @@
 Feature: Test
 
+  Scenario: Test 3 schema
+
+    Given I have set a connection to pivotal_tracker API service
+    When I request GET "services"
+    Then I expect Status code 200 for the SmokeTest
+    And I  format table schema format
+
   Scenario: Test
     Given I have set a connection to pivotal_tracker API service
     When I request GET "services"
     Then I expect Status code 200 for the SmokeTest
+    And I espect format :
+    """
+    [
+  {
+    "_id": "597f4452f179f912c08f32f6",
+    "type": "ExchangeServer",
+    "name": "Exchange Server 2016",
+    "version": "15.1 (Build 30225.42)",
+    "hostname": "server2012dc.arabitpro.local",
+    "username": "Administrator",
+    "deleteLockTime": 10
+  }
+   ]
+    """"""
+
+
+
+
--- features/step_roomandexchange/common_steps.rb
+++ features/step_roomandexchange/common_steps.rb
@@ -1,3 +1,7 @@
+require 'rspec'
+require 'json'
+require "json-schema"
+# require require_relative "../support/requests/roommanager/schema/meeting"
 
 Given(/^I have set a connection to pivotal_tracker API service$/) do
   require_relative '../../features/support/helpers/rest_client/api_rest_client'
@@ -6,4 +10,35 @@ end
 Then(/^I expect Status code (\d+) for the SmokeTest$/) do |http_code|
   p @http_response.code.to_s
   expect(@http_response.code.to_s).to eql(http_code.to_s)
+end
+
+And(/^I espect format :$/) do |http|
+    value=@http_response.body
+   json = JSON.parse(value)
+   rtr = JSON.parse(http)
+     # json.to_s == rtr.to_s
+    expect(json.to_s).to include(rtr.to_s)
+
+
+end
+
+
+
+And(/^I  format table schema format$/) do
+   p value =@http_response.body
+   # p @http_response.body.code
+  # p json = JSON.parse(value)
+
+   schema_directory = "#{Dir.pwd}/features/feature_roomandexchange/RoomManager/schema"
+  p schema_path = "#{schema_directory}/meeting.json"
+   JSON::Validator.validate(schema_path, @http_response.body)
+   # JSON::Validator.validate!(schema_path, value, strict: true)
+
+  # expect(response.status).to eq(200)
+  # expect(@http_response).to match_response_schema(:charge)
+  # expect(response).to match_json(<<-JSON)
+ # p  file = File.open('meeting.json')
+ #  p file.read
+  # JSON::Validator.validate('meeting.json', '{"a" : 5}')
+  # expect(json).to match_response_schema("meeting")
 end
